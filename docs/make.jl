1fda350187c657040a54c7004ca88b932856b04f
Dheepak Krishnamurthy <me@kdheepak.com>
2020-07-29 02:02:59 -0600

    Add documentation

---
+++ docs/make.jl
@@ -0,0 +1,21 @@
+using Documenter, TerminalUserInterfaces, DocumenterMarkdown
+
+cp(joinpath(@__DIR__, "../README.md"), joinpath(@__DIR__, "./src/index.md"), force=true, follow_symlinks=true)
+
+makedocs(
+         sitename="TerminalUserInterfaces.jl documentation",
+         format = Markdown()
+        )
+
+deploydocs(
+    repo = "github.com/kdheepak/TerminalUserInterfaces.jl.git",
+    deps = Deps.pip(
+                   "mkdocs==0.17.5",
+                   "mkdocs-material==2.9.4",
+                   "python-markdown-math",
+                   "pygments",
+                   "pymdown-extensions",
+                   ),
+    make = () -> run(`mkdocs build`),
+    target = "site",
+)
