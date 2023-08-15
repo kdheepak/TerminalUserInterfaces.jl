57a4b25efaf677c9ee84aa10df6f9eafad824a71
Dheepak Krishnamurthy <me@kdheepak.com>
2023-08-15 18:40:05 -0400

    feat: Update table dataframe example ✨

--- examples/dataframe.jl
+++ examples/dataframe.jl
@@ -5,15 +5,15 @@ using PalmerPenguins
 using Tables
 
 @kwdef mutable struct Model <: TUI.Model
-  df = DataFrame(PalmerPenguins.load())
+  table = DataFrame(PalmerPenguins.load())
   quit = false
 end
 
 function TUI.view(m::Model)
-  columnnames = Tables.columnnames(Tables.columns(m.df))
+  columnnames = Tables.columnnames(Tables.columns(m.table))
   N = length(columnnames)
   header = TUI.Row(; data = [TUI.Datum(; content = string(col)) for col in columnnames])
-  rows = map(Tables.rows(m.df)) do row
+  rows = map(Tables.rows(m.table)) do row
     TUI.Row(; data = [TUI.Datum(; content = string(item)) for item in row])
   end
   widths = [TUI.Percent(100 ÷ N) for _ in 1:N]
